6ac331c1605934fd692b18d3cb0b7f770b3757f9
ikefercol <ikefercol@alu.edu.gva.es>
2024-11-20 10:06:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/capitaleseuropajava/FirstFragment.java
@@ -0,0 +1,60 @@
+package com.example.capitaleseuropajava;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.capitaleseuropajava.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    ArrayList<String> listacapitales;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding.lvcapiales.setAdapter(adapter);
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        listacapitales = new ArrayList<>();
+        listacapitales.add("Madrid");
+        listacapitales.add("Londres");
+        listacapitales.add("Paris");
+
+        adapter = new ArrayAdapter<>(getContext(),
+                R.layout.capital_list_item,
+                R.id.lvcapiales ,
+                listacapitales);
+
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
